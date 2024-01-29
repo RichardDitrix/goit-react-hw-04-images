8acfe160db192154a4cfa32571eec8dc9a33b440
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2024-01-29 20:42:23 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,86 @@
-import { useState } from 'react';
-import { Searchbar } from '../Searchbar';
-import { ImageGallery } from '../ImageGallery';
+import { useEffect, useState } from 'react';
 
+import { fetchImages } from '../../services/images-api';
+import { ImageGallery } from '../ImageGallery';
+import { Loader } from '../Loader';
+import { Searchbar } from '../Searchbar';
+
+import { LoadButton } from '../ImageGallery/ImageGallery.styled';
 import { Container } from './App.styled';
+
+const STATUS = {
+  idle: 'idle',
+  pending: 'pending',
+  resolved: 'resolved',
+  rejected: 'rejected',
+};
 
 export const App = () => {
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [status, setStatus] = useState(STATUS.idle);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setQuery(search);
+    setImages([]);
+    setPage(1);
+  }, [search]);
+
+  useEffect(() => {
+    if (query === '') return;
+
+    async function searchImages() {
+      setStatus(STATUS.pending);
+
+      try {
+        const response = await fetchImages(query, page);
+        setImages(images => [...images, ...response.hits]);
+        setTotal(response.total);
+        setStatus(STATUS.resolved);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    searchImages();
+  }, [query, page]);
+
+  useEffect(() => {
+    if (page === 1) return;
+
+    window.scrollBy({
+      top: document.body.clientHeight,
+      behavior: 'smooth',
+    });
+  }, [page, images]);
+
+  const loadMore = () => {
+    setPage(page => page + 1);
+  };
 
   const onSubmit = search => {
     setSearch(search.trim());
   };
 
   return (
-    <Container>
-      <Searchbar onSubmit={onSubmit} />
-      <ImageGallery search={search} />
-    </Container>
+    <>
+      {status === STATUS.rejected && <div>{error.message}</div>}
+
+      <Container>
+        <Searchbar onSubmit={onSubmit} />
+        <ImageGallery images={images} />
+
+        {status === STATUS.resolved && images.length !== total && (
+          <LoadButton type="button" onClick={loadMore}>
+            Load more
+          </LoadButton>
+        )}
+      </Container>
+
+      {status === STATUS.pending && <Loader />}
+    </>
   );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,96 +1,29 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
-import { Loader } from 'components/Loader';
 
-import { fetchImages } from 'services/images-api';
+import { GalleryList } from './ImageGallery.styled';
 
-import { GalleryList, LoadButton } from './ImageGallery.styled';
-import { useEffect } from 'react';
-
-const STATUS = {
-  idle: 'idle',
-  pending: 'pending',
-  resolved: 'resolved',
-  rejected: 'rejected',
-};
-
-export const ImageGallery = ({ search }) => {
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState(1);
-
-  const [images, setImages] = useState([]);
-  const [total, setTotal] = useState(0);
-
-  const [status, setStatus] = useState(STATUS.idle);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setQuery(search);
-    setImages([]);
-    setPage(1);
-  }, [search]);
-
-  useEffect(() => {
-    if (query === '') return;
-
-    async function searchImages() {
-      setStatus(STATUS.pending);
-
-      try {
-        const response = await fetchImages(query, page);
-        setImages(images => [...images, ...response.hits]);
-        setTotal(response.total);
-        setStatus(STATUS.resolved);
-      } catch (error) {
-        setError(error);
-      }
-    }
-    searchImages();
-  }, [query, page]);
-
-  useEffect(() => {
-    if (page === 1) return;
-
-    window.scrollBy({
-      top: document.body.clientHeight,
-      behavior: 'smooth',
-    });
-  }, [page, images]);
-
-  const loadMore = () => {
-    setPage(page => page + 1);
-  };
-
+export const ImageGallery = ({ images = [] }) => {
   return (
-    <>
-      {status === STATUS.idle && <div></div>}
-
-      {status === STATUS.rejected && <div>{error.message}</div>}
-
-      <GalleryList>
-        {images.map(({ id, webformatURL, largeImageURL, tags }) => (
-          <ImageGalleryItem
-            key={id}
-            smallImage={webformatURL}
-            largeImage={largeImageURL}
-            alt={tags}
-          />
-        ))}
-      </GalleryList>
-
-      {status === STATUS.pending && <Loader />}
-
-      {status === STATUS.resolved && images.length !== total && (
-        <LoadButton type="button" onClick={loadMore}>
-          Load more
-        </LoadButton>
-      )}
-    </>
+    <GalleryList>
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          smallImage={webformatURL}
+          largeImage={largeImageURL}
+          alt={tags}
+        />
+      ))}
+    </GalleryList>
   );
 };
 
 ImageGallery.propTypes = {
-  search: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    }).isRequired
+  ),
 };
